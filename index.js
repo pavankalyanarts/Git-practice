26381a6df5b89b806abf5196e3d7d25430218c1c
pavankalyanarts <pavanteeda10@gmail.com>
2025-05-25 19:42:05 +0530

    version3

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 console.log("this is version2")
 console.log("learning Git module")
+
+function greetings(){
+
+    console.log("hey there it's Pavan here!!")
+
+}
+
+export default greetings
--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+import greetings from "."
+
 console.log("hello world, I am learning Git -- version control")
+
+function runServer(){
+    greetings()
+}
+
+runServer()
